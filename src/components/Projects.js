6ec52d8cd340748f9d02d3291d26a29d8810f11b
Fatima Saeed Khan <fatima.k215@gmail.com>
2022-10-28 15:17:36 +0500

    updated favicon

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,34 @@
+import { faScrewdriver, faWrench } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import AnimatedWord from "./AnimatedWord";
+import './Projects.scss';
+
 const Projects = () => {
+
+    const[letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+ 
+          setLetterClass('text-animate-hover')
+ 
+     }, [])
     return ( 
-        <div className="container">
+        <>
+        <div className="container projects-page">
+            <div className="textarea">
+                <h1>
+                    <AnimatedWord letterClass={letterClass} strArray={['Page']}/>&nbsp;
+                    <AnimatedWord letterClass={letterClass} strArray={['Under']}/>&nbsp;
+                    <AnimatedWord letterClass={letterClass} strArray={['Construction!']}/>&nbsp;
+                    <FontAwesomeIcon icon={faWrench} color="#8EE3EF"/>
+                </h1>
+            </div>
             
         </div>
+    <Loader type="line-scale-pulse-out"/>
+    </>
      );
 }
  
